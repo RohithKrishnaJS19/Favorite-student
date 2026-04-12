58d71bdbc40d55ecf891889ec4b880b552ccd7a4
RohithKrishnaJS19 <rohithkrishnajs1908@gmail.com>
2026-04-12 23:11:25 +0530

    Rohith created it

--- src/header.jsx
+++ src/header.jsx
@@ -5,10 +5,10 @@ function Header() {
     const location = useLocation()
     return (
         <div className="bg-white flex gap-14 p-3">
-                <Link to="/"><h1 className={`text-lg ${location.pathname === "/"? 
+                <Link to="/"><h1 className={`${location.pathname === "/"? 
                 "border-b-2 border-blue-600"
                             : ""}`}>List of Students</h1></Link>
-                <Link to="/favorite"><h1 className={`text-lg ${location.pathname == "/favorite"?
+                <Link to="/favorite"><h1 className={`${location.pathname == "/favorite"?
                     "border-b-2 border-blue-600":""
                 }`}>Favorite Students</h1></Link>
         </div >
